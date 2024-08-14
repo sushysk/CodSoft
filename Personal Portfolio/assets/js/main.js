96de53ac4a8467f4554a83537a655ca2ebcd2fd7
sushysk <125468722+sushysk@users.noreply.github.com>
2024-08-14 12:56:51 +0530

    Update main.js

--- Personal Portfolio/assets/js/main.js
+++ Personal Portfolio/assets/js/main.js
@@ -1,14 +1,15 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
 /* ----- NAVIGATION BAR FUNCTION ----- */
-function myMenuFunction(){
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction() {
   var menuBtn = document.getElementById("myNavMenu");
-
-  if(menuBtn.className === "nav-menu"){
-    menuBtn.className += " responsive";
+  if (menuBtn.className === "nav-menu") {
+      menuBtn.className += " responsive";
   } else {
-    menuBtn.className = "nav-menu";
+      menuBtn.className = "nav-menu";
   }
 }
+
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
 window.onscroll = function() {headerShadow()};
